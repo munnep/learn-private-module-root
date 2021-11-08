d9eedb3b25a797258c625faa7ce5a6af6bce5648
munnep <patrick.munne@hashicorp.com>
2021-11-08 12:18:17 +0100

    documentation

--- variables.tf
+++ variables.tf
@@ -1,11 +1,10 @@
 variable "region" {
-  description = "This is the cloud hosting region where your webapp will be deployed."
-}
-
+    default="us-east-1"
+    }
 variable "prefix" {
-  description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
-}
+    default = "dev-test"
+    }
 
 variable "name" {
-  description = "Your name to attach to the webapp address"
-}
+    default = "patrick"
+    }
